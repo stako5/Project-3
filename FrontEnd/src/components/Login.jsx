619b336dba292582bb666dbfb875819b917f6076
Alex Morgan <am2morgan7@gmail.com>
2025-08-25 19:35:55 +0000

    styling

--- FrontEnd/src/components/Login.jsx
+++ FrontEnd/src/components/Login.jsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
+import NavBar from "./NavBar";
 
 function Login() {
-  return <></>;
+  return (
+    <>
+      <header className="header">
+        <h1>STOPIFY</h1>
+        <NavBar />
+      </header>
+    </>
+  );
 }
 
 export default Login;
--- FrontEnd/src/components/Playlist.jsx
+++ FrontEnd/src/components/Playlist.jsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
+import NavBar from "./NavBar";
 
 function Playlist() {
-  return <></>;
+  return (
+    <>
+      <header className="header">
+        <h1>STOPIFY</h1>
+        <NavBar />
+      </header>
+    </>
+  );
 }
 
 export default Playlist;
