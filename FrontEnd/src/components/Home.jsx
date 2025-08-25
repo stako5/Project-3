77609ce7e3c82d60d6ccfd9e7b72a7fbeeb1c6a2
Alex Morgan <am2morgan7@gmail.com>
2025-08-25 15:30:16 +0000

    updated home page

---
+++ FrontEnd/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import NavBar from "./NavBar";
+
+
+function Home() {
+  return (
+    <>
+    <div>
+          <h1>STOPIFY</h1>
+          <NavBar/>
+        </div>
+    </>
+  )
+}
+
+export default Home;
