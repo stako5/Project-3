a52b9001e391282e7b784e06b133bc02f9bd33f4
stako5 <bankhead089@gmail.com>
2025-08-26 23:59:48 +0200

    Update backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.DBPORT
+const port = process.env.PORT || 8000
 const cors = require('cors');
 const knex = require('knex')(require('./knexfile.js')['development']);
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: { "/api": "http://backend:3001" },
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      proxy: { "/api": env.VITE_BACKEND_URL || "http://backend:8000" },
+    },
+  };
 });
