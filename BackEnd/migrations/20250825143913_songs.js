e34610c8a11f50244c2ed87a678d4d5638a6901b
Alex Morgan <am2morgan7@gmail.com>
2025-08-26 16:30:34 +0000

    starting styling

--- BackEnd/migrations/20250825143913_songs.js
+++ BackEnd/migrations/20250825143913_songs.js
@@ -2,17 +2,18 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  return knex.schema.createTable('songs', table => {
-    table.increments()
-    table.string('genre', 256);
-  })
+exports.up = function (knex) {
+  return knex.schema.createTable("songs", (table) => {
+    table.increments();
+    table.string("genre", 256);
+    table.string("songname");
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('songs');
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("songs");
 };
--- BackEnd/seeds/01_songs.js
+++ BackEnd/seeds/01_songs.js
@@ -1,20 +1,23 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-const { faker } = require('@faker-js/faker');
-const fakeSongs = []
-const desiredFakeSongs = 10
-let fakeId = 1
+const { faker } = require("@faker-js/faker");
+const fakeSongs = [];
+const desiredFakeSongs = 10;
+let fakeId = 1;
 
 for (let i = 0; i < desiredFakeSongs; i++) {
-  fakeSongs.push({id: fakeId, genre: faker.music.genre(),  })
+  fakeSongs.push({
+    id: fakeId,
+    genre: faker.music.genre(),
+    songname: faker.music.songName(),
+  });
   fakeId++;
 }
 
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('songs').del()
-  await knex('songs').insert(fakeSongs);
+  await knex("songs").del();
+  await knex("songs").insert(fakeSongs);
 };
-
