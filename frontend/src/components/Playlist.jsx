68e916d97295109b475300d3a6e588ecb1764a85
michael-leibolt <mike.leibolt1.1@gmail.com>
2025-08-27 17:52:48 -0400

    end of day

--- frontend/src/components/Playlist.jsx
+++ frontend/src/components/Playlist.jsx
@@ -1,7 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 
 function Playlist() {
+  const [playlist, setPlaylist] = useState([{id: 0}])
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_BASE_URL}/api/playlists`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      var playlist = data.filter((play) => play.id == 1)
+      console.log(playlist)
+      return playlist;
+    })
+    .then(output => setPlaylist(output))
+  }, [])
   return (
     <>
       <header className="header">
@@ -9,7 +21,11 @@ function Playlist() {
         <NavBar />
       </header>
       <main>
-        <h1>test</h1>
+
+        {console.log(playlist[0])}
+        <h1>{playlist[0].name}</h1>
+        <p>{playlist[0].description}</p>
+        <p>{playlist[0].songs_array}</p>
       </main>
       <footer className="footer">
         &copy; 2025 STOPIFY. All rights reserved.
