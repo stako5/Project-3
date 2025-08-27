cd9155ec05b83cda3f7232957a6417c90727d0e1
stako5 <bankhead089@gmail.com>
2025-08-27 16:54:54 +0200

    Updat docker file with knex

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -11,11 +11,17 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: process.env.PGHOST || 'localhost',
-      user: process.env.PGUSER || 'postgres',
-      password: process.env.PGPASSWORD || 'postgres',
-      database: process.env.PGDATABASE || 'mydb',
-      port: process.env.PGPORT || 5432
+      host: process.env.DB_HOST || process.env.PGHOST || 'localhost',
+      user: process.env.DB_USER || process.env.PGUSER || 'postgres',
+      password: process.env.DB_PASSWORD || process.env.PGPASSWORD || 'postgres',
+      database: process.env.DB_DATABASE || process.env.PGDATABASE || 'mydb',
+      port: process.env.DB_PORT || process.env.PGPORT || 5432
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
     }
   }
 };
