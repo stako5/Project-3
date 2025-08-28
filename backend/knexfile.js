f630ac272063609ce2393adf726dc2a7c43dc07e
michael-leibolt <mike.leibolt1.1@gmail.com>
2025-08-28 13:00:16 -0400

    last push

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -8,22 +8,22 @@ require('dotenv').config();
 
 module.exports = {
 
-  development: {
-    client: 'pg',
-    connection: {
-      host: 'localhost',
-      user: 'postgres',
-      password: 'docker',
-      database: 'mydb',
-      port: 5432
-    },
-    migrations: {
-      directory: './migrations'
-    },
-    seeds: {
-      directory: './seeds'
-    }
-  }
+  // development: {
+  //   client: 'pg',
+  //   connection: {
+  //     host: 'localhost',
+  //     user: 'postgres',
+  //     password: 'docker',
+  //     database: 'mydb',
+  //     port: 5432
+  //   },
+  //   migrations: {
+  //     directory: './migrations'
+  //   },
+  //   seeds: {
+  //     directory: './seeds'
+  //   }
+  // }
 // Docker Command for local env
 // docker run --rm --name pg-docker -e POSTGRES_PASSWORD=docker -d -p 5432:5432 \
 // -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres
@@ -38,20 +38,20 @@ module.exports = {
 // ~Project-3/backend: cd ../frontend
 // ~Project-3/frontend: npm run dev
 
-  // development: {
-  //   client: 'pg',
-  //   connection: {
-  //     host: process.env.DB_HOST || process.env.PGHOST || 'localhost',
-  //     user: process.env.DB_USER || process.env.PGUSER || 'postgres',
-  //     password: process.env.DB_PASSWORD || process.env.PGPASSWORD || 'postgres',
-  //     database: process.env.DB_DATABASE || process.env.PGDATABASE || 'mydb',
-  //     port: process.env.DB_PORT || process.env.PGPORT || 5432
-  //   },
-  //   migrations: {
-  //     directory: './migrations'
-  //   },
-  //   seeds: {
-  //     directory: './seeds'
-  //   }
-  // }
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST || process.env.PGHOST || 'localhost',
+      user: process.env.DB_USER || process.env.PGUSER || 'postgres',
+      password: process.env.DB_PASSWORD || process.env.PGPASSWORD || 'postgres',
+      database: process.env.DB_DATABASE || process.env.PGDATABASE || 'mydb',
+      port: process.env.DB_PORT || process.env.PGPORT || 5432
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  }
 };
